df6f0af21efb19a28202d651281e95fc0832caee
Vinh Huynh <vinh.huynh@blueberrylabs.com>
2022-07-23 08:58:06 +0700

    add log

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -9,38 +9,45 @@ logger.info(`======= DB_PORT: ${DB_PORT} =====`);
 logger.info(`======= DB_USER: ${DB_USER} =====`);
 logger.info(`=================================`);
 
-const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-  dialect: 'mysql',
-  host: DB_HOST,
-  port: +DB_PORT,
-  timezone: '+09:00',
-  define: {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
-    underscored: true,
-    freezeTableName: true,
-  },
-  pool: {
-    min: 0,
-    max: 5,
-  },
-  logQueryParameters: NODE_ENV === 'development',
-  logging: (query, time) => {
-    logger.info(time + 'ms' + ' ' + query);
-  },
-  benchmark: true,
-});
-
+let sequelize;
 try {
+  sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+    dialect: 'mysql',
+    host: DB_HOST,
+    port: +DB_PORT,
+    timezone: '+09:00',
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      underscored: true,
+      freezeTableName: true,
+    },
+    pool: {
+      min: 0,
+      max: 5,
+    },
+    logQueryParameters: NODE_ENV === 'development',
+    logging: (query, time) => {
+      logger.info(time + 'ms' + ' ' + query);
+    },
+    benchmark: true,
+  });
+
   sequelize.authenticate();
 } catch (error) {
   logger.error(error);
 }
 
-const DB = {
-  Users: UserModel(sequelize),
-  sequelize, // connection instance (RAW queries)
-  Sequelize, // library
-};
+let DB;
+
+try {
+  DB = {
+    Users: UserModel(sequelize),
+    sequelize, // connection instance (RAW queries)
+    Sequelize, // library
+  };
+} catch (error) {
+  logger.error(error);
+}
 
 export default DB;
